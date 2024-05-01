c8271f85d1c0d338fe56303e06674192f507aa9a
Vagelis Xaniadakis <sdi1700280@di.uoa.gr>
2024-05-01 09:32:18 +0300

    add javadocs to annotation configuration

--- src/main/java/com/money/transfer/app/annotation/LogAspect.java
+++ src/main/java/com/money/transfer/app/annotation/LogAspect.java
@@ -6,15 +6,38 @@ import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.annotation.*;
 import org.springframework.stereotype.Component;
 
+/**
+ * Implements centralized logging for transactional operations using AOP.
+ *
+ * Utilizes Spring AOP to intercept transaction methods
+ * before,
+ * after normally returning and
+ * after throwing exception scenarios
+ * to log useful information about the course of the current transaction request.
+ */
 @Slf4j
 @Component
 @Aspect
 @RequiredArgsConstructor
 public class LogAspect {
 
+    /**
+     * Defines a pointcut that targets
+     * methods annotated with {@link LogRequestCourse}
+     * and methods within classes annotated with {@link LogRequestCourse},
+     * which also accept a {@link TransferRequestBody}.
+     *
+     * @param requestBody The details of the transaction request
+     */
     @Pointcut("(@within(LogRequestCourse) || @annotation(LogRequestCourse)) && args(requestBody)")
     public void logRequest(TransferRequestBody requestBody) {}
 
+    /**
+     * Executes before the target method runs.
+     * Logs an informational message indicating the initiation of a transfer.
+     *
+     * @param requestBody The details of the transaction request
+     */
     @Before(value = "logRequest(requestBody)", argNames = "requestBody")
     public void logBeforeMethod(TransferRequestBody requestBody) {
         log.info("Initiating transfer from account {} to account {} for amount {} {}",
@@ -22,6 +45,12 @@ public class LogAspect {
                 requestBody.getAmount(), requestBody.getCurrency());
     }
 
+    /**
+     * Executes only if the target method completes successfully.
+     * Logs a success message once the transaction is completed.
+     *
+     * @param requestBody The details of the transaction request
+     */
     @AfterReturning(pointcut = "logRequest(requestBody)", argNames = "requestBody")
     public void logAfterMethodReturning(TransferRequestBody requestBody) {
         log.info("Transfer of {}{} from account {} to account {} completed successfully.",
@@ -29,6 +58,12 @@ public class LogAspect {
                 requestBody.getSourceAccountId(), requestBody.getTargetAccountId());
     }
 
+    /**
+     *  Captures any exceptions thrown by the target method.
+     *  Logs an error message containing the exception type and message.
+     *
+     * @param requestBody The details of the transaction request
+     */
     @AfterThrowing(pointcut = "logRequest(requestBody)", throwing = "ex", argNames = "requestBody,ex")
     public void logAfterThrowingException(TransferRequestBody requestBody, Throwable ex) {
         log.error("{} during transfer from account {} to account {}",
--- src/main/java/com/money/transfer/app/annotation/LogRequestCourse.java
+++ src/main/java/com/money/transfer/app/annotation/LogRequestCourse.java
@@ -5,6 +5,11 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Annotation to tag methods and classes for centralized logging via AOP.
+ * Enables logging at runtime by serving as a pointcut indicator for
+ * intercepting method executions.
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(value = {ElementType.METHOD, ElementType.TYPE})
 public @interface LogRequestCourse {
